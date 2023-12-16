49bec4e07dd8cb7ec1aef48f0ae5921f97f3b55c
5000vw <isaackiplangat1999@gmail.com>
2023-12-16 13:03:53 +0300

    completed asignment

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,16 @@
 def write_file(file_name, file_content):
-    pass
+    with open (f'{file_name}.txt', 'w') as text_file:
+        text_file.write(file_content)
+    
 
 def append_file(file_name, append_content):
-    pass
+    with open (f'{file_name}.txt', 'a') as text_file:
+        text_file.write(append_content)
 
 def read_file(file_name):
-    pass
+    with open(f"{file_name}.txt", 'r') as text_file:
+        return text_file.read()
+
+write_file(file_name="logfile", file_content="Log 1: 5 bananas added" )
+append_file(file_name="logfile", append_content="Log 2: 3 bananas subtracted")
+read_file(file_name="logfile")
